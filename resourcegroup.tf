528a22811158b4ed1ff5007f34cc377b57b120c1
awseng19 <maryam.r10843@gmail.com>
2025-03-02 22:55:02 -0500

    Create resourcegroup.tf

---
+++ resourcegroup.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "mcit420zz5um" {
+  name     = "mcit_resource_group_w24"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "mcit420zz5um2" {
+  name     = "mcit_resource_group_w24_2"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "mcit420zz5um3" {
+  name     = "mcit_resource_group_w24_3"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "mcit420zz5um4" {
+  name     = "mcit_resource_group_w24_4"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "mcit_resource_group_w24_5"
+  location = "canadacentral"
+}
