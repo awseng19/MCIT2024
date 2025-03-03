327474e793bc26397b41a056f3d032777e2a4f97
awseng19 <maryam.r10843@gmail.com>
2025-03-02 22:47:10 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "subscription_id"{
+  type=string
+}
+
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "account_tier"{
+  type=string
+ default="Premium"
+}
+variable "admin"{
+type=string
+}
+variable "password"{
+type=string
+}
