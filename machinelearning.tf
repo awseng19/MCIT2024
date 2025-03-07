75db18cab047e2425b2a3947dfd963875039bfe7
awseng19 <maryam.r10843@gmail.com>
2025-03-07 13:35:45 -0500

    Update machinelearning.tf

--- machinelearning.tf
+++ machinelearning.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "mlrg-m" {
-  name     = "ml_rg-resources"
+  name     = "ml_rg-resources-maryam"
   location = "West Europe"
 }
 
